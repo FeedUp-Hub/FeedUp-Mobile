9bd51dd6abb53ac40fbd1f1c954184300a0ab227
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-05-21 21:15:18 -0300

    Updated Routes Added

---
+++ routes/DrawerNavigator.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import BottomTabNavigator from './BottomTabNavigator';
+import CustomDrawerContent from '../routes/CustomDrawerContent';
+import Metrics from '../layouts/Metrics';
+import NozesTrading from '../extrasFuture/NozesTrading';
+import Login from '../layouts/LoginScreen';
+
+const Drawer = createDrawerNavigator();
+
+const DrawerNavigator = () => (
+  <Drawer.Navigator drawerContent={(props) => <CustomDrawerContent {...props} />}>
+    <Drawer.Screen name="Home" component={BottomTabNavigator} />
+    <Drawer.Screen name="Métricas" component={Metrics} />
+    <Drawer.Screen name="Ioasys Trade (soon)" component={NozesTrading} />
+    <Drawer.Screen name="Logout" component={Login} />
+  </Drawer.Navigator>
+);
+
+export default DrawerNavigator;
