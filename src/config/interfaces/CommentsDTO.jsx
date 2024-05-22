35cfbe175dd5ee77117eb0eb26702282f95c6a05
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-05-21 20:18:49 -0400

    Delete src/config/interfaces directory

--- src/config/interfaces/CommentsDTO.jsx
+++
@@ -1,9 +0,0 @@
-export interface CommentsDTO {
-  id: string;
-  id_feedup: string;
-  id_usercommented: string;
-  message: string;
-  like: number;
-  created_at: string;
-  updated_at?: string;
-}
--- src/config/interfaces/MoodsDTO.jsx
+++
@@ -1,7 +0,0 @@
-export interface MoodsDTO {
-  id: string;
-  id_user: string;
-  moods: number;
-  created_at: string;
-  updated_at?: string;
-}
