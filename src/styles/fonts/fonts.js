62a3e700200aaa522a0c560954b1391436ad3aa3
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-05-21 21:07:16 -0400

    Delete src/styles/fonts/fonts.js

--- src/styles/fonts/fonts.js
+++
@@ -1,9 +0,0 @@
-// Declaração das fontes Poppins
-const FONTS = {
-  REGULAR_POPPINS: 'Poppins-Regular',
-  BOLD_POPPINS: 'Poppins-Bold'
-};
-
-export type FONT_TYPE = typeof FONTS;
-
-export default FONTS;
